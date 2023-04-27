498b27c5687009eef9d159e361415a0142050873
Mohammad Hashem <hashimhanify@gmail.com>
2023-04-27 06:19:04 +0430

    installing bodyParser and express validator

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 const port = 5000;
 const app = express();
 
